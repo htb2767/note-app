22305f41fd69f7e1132726d52cbd604489b56291
Huong Bui <hbui23@gmu.edu>
2024-01-07 10:25:44 -0500

    cor

--- mynotes/settings.py
+++ mynotes/settings.py
@@ -125,6 +125,8 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",  # Add the origin of your React app
+    "http://localhost:3000",
+    "https://note-qhh9.onrender.com"
+    # Add the origin of your React app
 ]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
